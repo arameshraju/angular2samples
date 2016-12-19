ee5afcf4a21011e984a860d98008c07ee626c9c0
Ramesh Raju Alluri <g801691@ad.syniverse.com>
2016-12-19 14:44:46 +0530

    Base ng-2 structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angular2samples
 Angular2samples
+A basic Angualrjs guied build  based on my blog  http://whatilearned.in/2016/12/16/quick-start-guide-angularjs2/  
---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+  import { Component } from '@angular/core';
+
+  @Component({
+  selector:'my-app',
+  template : '<h1>Welocme to {{name}} </h1>'
+  })
+  export class appComponent{
+    name="My Ng2 App";
+  }
